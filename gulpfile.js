a693abb584d24a0743e4c1478c0253c190fc3c70
Kelsey Jackson <kelseythej@gmail.com>
2016-02-14 19:26:57 -0600

    Fixed errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,18 @@
 'use strict';
 
 var gulp = require('gulp'),
-    jade = require('gulp-jade'),
     sass = require('gulp-sass');
 
 
 gulp.task('compileSass', function() {
-  gulp.src('./scss/app.scss')
+  gulp.src('./scss/styles.scss')
   .pipe(sass())
   .pipe(gulp.dest('./css'));
-});
-gulp.task('compileJade', function(){
-  gulp.src('./jade/index.jade')
-  .pipe(jade())
-  .pipe(gulp.dest('.'));
 });
 
 
 gulp.task('watchFiles', function(){
   gulp.watch('scss/**/*.scss', ['compileSass']);
-  gulp.watch('jade/**/*.jade', ['compileJade']);
 });
 
 gulp.task('serve', ['watchFiles']);
